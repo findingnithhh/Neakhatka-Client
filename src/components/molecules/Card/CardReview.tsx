d8642534d14c60ec02c72d4ec1f315d59b5a4187
SOMKEY\ROG <manithjr1331@gmail.com>
2024-03-01 00:45:16 +0700

    complated card review

--- src/components/molecules/Card/CardReview.tsx
+++ src/components/molecules/Card/CardReview.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const CardReview = () => {
   return (
     <>
-      <div className="w-full h-[700px] mt-10 bg-[#F8F8F8] font-Poppins">
-        <div className="w-[1200px] h-[200px] mx-auto">
+      <div className="w-full h-auto mt-10 bg-[#efefef] font-Poppins">
+        <div className="w-[1200px] h-auto mx-auto">
           {/* title */}
           <Typography
             className="ml-10 pt-10 font-semibold capitalize tracking-wide"
@@ -85,6 +85,80 @@ const CardReview = () => {
                   purus in adipiscing vel dolor donec. In in id ultrices
                   maecenas et. Nibh faucibus neque.
                 </q>
+              </div>
+            </div>
+          </div>
+          {/* card */}
+          <div className="grid grid-cols-2 grid-container">
+            <div className="m-10 flex justify-center items-center float-right">
+              <div className="w-[450px]">
+                <q>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus
+                  purus in adipiscing vel dolor donec. In in id ultrices
+                  maecenas et. Nibh faucibus neque.
+                </q>
+              </div>
+            </div>
+            <div className="m-10 float-right">
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./person.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./person.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./person.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
