0730f69f86dd7c46e533b54a0c9c594baba6537b
SOMKEY\ROG <manithjr1331@gmail.com>
2024-03-01 14:56:39 +0700

    complated dynamic card review

---
+++ src/components/molecules/Card/BackUp.tsx
@@ -0,0 +1,175 @@
+import { Button, Icon, Typography } from "@/components/atoms";
+import Image from "next/image";
+import React from "react";
+const BackUp = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center mt-12">
+        <Button colorOutline="primary" size="md" rounded="lg">
+          See more
+        </Button>
+      </div>
+      <div className="w-full h-auto mt-10 bg-[#efefef] font-Poppins">
+        <div className="w-[1200px] h-auto mx-auto">
+          {/* title */}
+          <Typography
+            className="ml-10 pt-10 font-semibold capitalize tracking-wide"
+            fontSize="2xl"
+          >
+            What our client say?
+          </Typography>
+          {/* card */}
+          <div className="grid grid-cols-2">
+            <div className="mx-10">
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] ml-20 my-16">
+                <div className="flex items-center">
+                  <Image
+                    src="./userProfile/user.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tang Sapho</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      React Js at Cellcard
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px]">
+                <div className="flex items-center">
+                  <Image
+                    src="./userProfile/user1.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] ml-20 my-16">
+                <div className="flex items-center">
+                  <Image
+                    src="./person.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="m-10 flex justify-center items-center">
+              <div className="w-[450px]">
+                <q>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus
+                  purus in adipiscing vel dolor donec. In in id ultrices
+                  maecenas et. Nibh faucibus neque.
+                </q>
+              </div>
+            </div>
+          </div>
+          {/* card */}
+          <div className="grid grid-cols-2 grid-container">
+            <div className="m-10 flex justify-center items-center">
+              <div className="w-[450px]">
+                <q>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus
+                  purus in adipiscing vel dolor donec. In in id ultrices
+                  maecenas et. Nibh faucibus neque.
+                </q>
+              </div>
+            </div>
+            <div className="mx-10 float-right">
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./userProfile/user1.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./userProfile/user.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
+                <div className="flex items-center">
+                  <Image
+                    src="./person.svg"
+                    alt="person"
+                    width={54}
+                    height={54}
+                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                  />
+                  <div className="ml-3">
+                    <Typography>Tieng Kimlang</Typography>
+                    <Typography
+                      className="text-[#ADB5BD] tracking-wide"
+                      fontSize="xs"
+                    >
+                      UX/UI designer at SabaiCode
+                    </Typography>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BackUp;
--- src/components/molecules/Card/CardReview.tsx
+++ src/components/molecules/Card/CardReview.tsx
@@ -1,8 +1,60 @@
-import { Button, Icon, Typography } from "@/components/atoms";
+"use client";
+import React, { useState } from "react";
+import { Button, Typography } from "@/components/atoms";
 import Image from "next/image";
-import React from "react";
 
-const CardReview = () => {
+interface UserProps {
+  id: number;
+  name: string;
+  position: string;
+  description: string;
+  image: string;
+}
+
+const defaultDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus purus in adipiscing vel dolor donec. In in id ultrices maecenas et. Nibh faucibus neque, ipsum volutpat nullam turpis. Ultrices libero sit rhoncus ac et viverra lobortis nisl, interdum.";
+
+const CardReview: React.FC = () => {
+  const [expanded, setExpanded] = useState<number | null>(null);
+  const [selectedDescription, setSelectedDescription] =
+    useState<string>(defaultDescription);
+
+  const user: UserProps[] = [
+    {
+      id: 1,
+      name: "Tieng Kimlang",
+      position: "React Js at SabaiCode",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus purus in adipiscing vel dolor donec. In in id ultrices maecenas et. Nibh faucibus neque, ipsum volutpat nullam turpis. Ultrices libero sit rhoncus ac et viverra lobortis nisl, interdum.",
+      image: "./userProfile/user.svg",
+    },
+    {
+      id: 2,
+      name: "Tang Sapho",
+      position: "Vue Js at SabaiCode",
+      description: "dak jol hx nah pu !!! ouyyy thom gha ",
+      image: "./userProfile/user1.svg",
+    },
+    {
+      id: 3,
+      name: "Chhun Rathna",
+      position: "UX/UI Js at SabaiCode",
+      description:
+        " Nibh faucibus neque, ipsum volutpat nullam turpis. Ultrices libero sit rhoncus ac et viverra lobortis nisl, interdum.",
+      image: "./userProfile/user2.svg",
+    },
+  ];
+
+  const handleToggle = (id: number, description: string) => {
+    if (expanded === id) {
+      setExpanded(null);
+      setSelectedDescription(defaultDescription);
+    } else {
+      setExpanded(id);
+      setSelectedDescription(description);
+    }
+  };
+
   return (
     <>
       <div className="flex justify-center items-center mt-12">
@@ -12,157 +64,48 @@ const CardReview = () => {
       </div>
       <div className="w-full h-auto mt-10 bg-[#efefef] font-Poppins">
         <div className="w-[1200px] h-auto mx-auto">
-          {/* title */}
           <Typography
             className="ml-10 pt-10 font-semibold capitalize tracking-wide"
             fontSize="2xl"
           >
             What our client say?
           </Typography>
-          {/* card */}
           <div className="grid grid-cols-2">
-            <div className="mx-10">
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] ml-20 my-16">
-                <div className="flex items-center">
-                  <Image
-                    src="./userProfile/user.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tang Sapho</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      React Js at Cellcard
-                    </Typography>
+            <div className="mx-auto">
+              {user.map((items, index) => (
+                <div
+                  key={items.id}
+                  className={`w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] my-16 ${
+                    expanded === items.id ? "bg-gray-100" : ""
+                  } ${index === 0 || index === 2 ? "ml-20" : ""}`}
+                  onClick={() => handleToggle(items.id, items.description)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <div className="flex items-center">
+                    <Image
+                      src={items.image}
+                      alt="person"
+                      width={54}
+                      height={54}
+                      className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
+                    />
+                    <div className="ml-3">
+                      <Typography>{items.name}</Typography>
+                      <Typography
+                        className="text-[#ADB5BD] tracking-wide"
+                        fontSize="xs"
+                      >
+                        {items.position}
+                      </Typography>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px]">
-                <div className="flex items-center">
-                  <Image
-                    src="./userProfile/user1.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tieng Kimlang</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      UX/UI designer at SabaiCode
-                    </Typography>
-                  </div>
-                </div>
-              </div>
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] ml-20 my-16">
-                <div className="flex items-center">
-                  <Image
-                    src="./person.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tieng Kimlang</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      UX/UI designer at SabaiCode
-                    </Typography>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
-            <div className="m-10 flex justify-center items-center">
-              <div className="w-[450px]">
-                <q>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus
-                  purus in adipiscing vel dolor donec. In in id ultrices
-                  maecenas et. Nibh faucibus neque.
-                </q>
-              </div>
-            </div>
-          </div>
-          {/* card */}
-          <div className="grid grid-cols-2 grid-container">
-            <div className="m-10 flex justify-center items-center">
-              <div className="w-[450px]">
-                <q>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus
-                  purus in adipiscing vel dolor donec. In in id ultrices
-                  maecenas et. Nibh faucibus neque.
-                </q>
-              </div>
-            </div>
-            <div className="mx-10 float-right">
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
-                <div className="flex items-center">
-                  <Image
-                    src="./userProfile/user1.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tieng Kimlang</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      UX/UI designer at SabaiCode
-                    </Typography>
-                  </div>
-                </div>
-              </div>
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] float-right">
-                <div className="flex items-center">
-                  <Image
-                    src="./userProfile/user.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tieng Kimlang</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      UX/UI designer at SabaiCode
-                    </Typography>
-                  </div>
-                </div>
-              </div>
-              <div className="w-[340px] h-[100px] shadow-xl flex items-center px-3 rounded-[6px] mr-20 my-16 float-right">
-                <div className="flex items-center">
-                  <Image
-                    src="./person.svg"
-                    alt="person"
-                    width={54}
-                    height={54}
-                    className="w-[60px] h-[60px] flex items-center rounded-full object-cover"
-                  />
-                  <div className="ml-3">
-                    <Typography>Tieng Kimlang</Typography>
-                    <Typography
-                      className="text-[#ADB5BD] tracking-wide"
-                      fontSize="xs"
-                    >
-                      UX/UI designer at SabaiCode
-                    </Typography>
-                  </div>
+            <div className="flex justify-center items-center">
+              <div className="w-[450px] h-[500px] flex justify-center items-center">
+                <div className="w-[450px]">
+                  <q>{selectedDescription}</q>
                 </div>
               </div>
             </div>
