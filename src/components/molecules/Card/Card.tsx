cc10a30f65ff417af95fc7b0c189713120084833
SOMKEY\ROG <manithjr1331@gmail.com>
2024-04-21 14:34:13 +0700

    completed favorite page delete

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -3,24 +3,25 @@ import { Icon } from "@/components";
 import { Typography } from "../../atoms/Typography";
 import React, { useState } from "react";
 import Image from "next/legacy/image";
-import { DetailCard } from "@/Types/DetailCard";
 import Link from "next/link";
+import { DetailCard } from "@/Types/DetailCard";
 
 interface CardProps {
   className?: string;
   data: DetailCard;
   iconType?: "star" | "close";
+  onDelete?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const Card: React.FC<CardProps> = ({
   className = "",
   data,
   iconType = "star",
+  onDelete,
 }) => {
   const {
     id,
     companyName,
-    companyLogo,
     peopleAmount,
     jobTitle,
     salary,
@@ -29,6 +30,18 @@ const Card: React.FC<CardProps> = ({
     DeadLine,
   } = data;
 
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorited((prevState) => !prevState);
+  };
+
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    if (onDelete) {
+      onDelete(e);
+    }
+  };
   const [starred, setStarred] = useState(false);
 
   const handleStarClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -38,84 +51,93 @@ const Card: React.FC<CardProps> = ({
   };
 
   return (
-    <>
+    <div
+      className={`h-auto rounded-xl shadow-lg p-5 font-Poppins ${className}`}
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+    >
       <Link href={`/detail/${id}`}>
-        <div className="h-auto rounded-xl shadow-lg p-5 font-Poppins cursor-pointer">
-          <div className="flex justify-between items-center">
-            {/* image */}
-            <div className="flex">
-              <Image
-                className="rounded-full"
-                src="/company.svg"
-                alt="company logo"
-                width={48}
-                height={48}
-              ></Image>
-              {/* title */}
-              <div className="font-Poppins ml-2">
-                <Typography>{companyName}</Typography>
-                <Typography fontSize="sm" className="text-gray-500">
-                  {peopleAmount}
-                </Typography>
-              </div>
-            </div>
-            {/* favorite icon */}
-            <div onClick={handleStarClick}>
-              {iconType === "star" ? (
-                <Icon
-                  label={starred ? "StarFill" : "Star"}
-                />
-              ) : (
-                <Icon label="Close" />
-              )}
+        <div className="flex justify-between items-center">
+          <div className="flex">
+            <Image
+              className="rounded-full"
+              src="/company.svg"
+              alt="company logo"
+              width={48}
+              height={48}
+            />
+            <div className="font-Poppins ml-2">
+              <Typography>{companyName}</Typography>
+              <Typography fontSize="sm" className="text-gray-500">
+                {peopleAmount}
+              </Typography>
             </div>
           </div>
-          <div className="flex">
+          {/* favorite */}
+          <div onClick={handleStarClick}>
+            {iconType === "star" ? (
+              <button onClick={toggleFavorite}>
+                <Icon
+                  label={isFavorited ? "StarFill" : "Star"}
+                  colorBackground={isFavorited ? "yellow" : "black"}
+                />
+              </button>
+            ) : (
+              <button
+                onClick={(e) => {
+                  e.preventDefault(); // Prevent default button behavior
+                  handleDelete(e);
+                }}
+              >
+                <Icon label="Close" />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="flex">
+          <div>
+            <Typography className="mt-5" fontSize="sm">
+              {jobTitle}
+            </Typography>
+            <Typography className="text-gray-500" fontSize="sm">
+              {salary}
+            </Typography>
             <div>
-              {/* position name */}
-              <Typography className="mt-5" fontSize="sm">
-                {jobTitle}
-              </Typography>
-              <Typography className="text-gray-500" fontSize="sm">
-                {salary}
-              </Typography>
-              <div>
-                <Typography className="mt-2" fontSize="sm">
-                  Employment
-                </Typography>
-                <Typography className="text-gray-500" fontSize="sm">
-                  <div className="flex">
-                    <Icon className="mr-2" label="Bag" size="sm" />
-                    {Emploment}
-                  </div>
-                </Typography>
-              </div>
-            </div>
-            <div className="mx-auto ">
-              {/* location */}
-              <Typography className="mt-5" fontSize="sm">
-                Location
-              </Typography>
-              <Typography className="text-gray-500" fontSize="sm">
-                <div className="flex">
-                  <Icon className="mr-2" label="Location" size="sm" />
-                  {location}
-                </div>
-              </Typography>
               <Typography className="mt-2" fontSize="sm">
-                Deadline
+                Employment
               </Typography>
               <Typography className="text-gray-500" fontSize="sm">
                 <div className="flex">
-                  <Icon className="mr-2" label="Date" size="sm" />
-                  {DeadLine}
+                  <Icon className="mr-2" label="Bag" size="sm" />
+                  {Emploment}
                 </div>
               </Typography>
             </div>
+          </div>
+          <div className="mx-auto">
+            <Typography className="mt-5" fontSize="sm">
+              Location
+            </Typography>
+            <Typography className="text-gray-500" fontSize="sm">
+              <div className="flex">
+                <Icon className="mr-2" label="Location" size="sm" />
+                {location}
+              </div>
+            </Typography>
+            <Typography className="mt-2" fontSize="sm">
+              Deadline
+            </Typography>
+            <Typography className="text-gray-500" fontSize="sm">
+              <div className="flex">
+                <Icon className="mr-2" label="Date" size="sm" />
+                {DeadLine}
+              </div>
+            </Typography>
           </div>
         </div>
       </Link>
-    </>
+    </div>
   );
 };
 
